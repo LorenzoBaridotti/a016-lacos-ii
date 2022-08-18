2d0268b8ec7e682fc03e573ea8d3520aa39d5a9d
LorenzoBaridotti <lorenzo.baridotti@hotmail.com>
2022-08-18 20:39:32 -0300

    Exercícios a016

---
+++ exercicio2/script.js
@@ -0,0 +1,21 @@
+let tabuada = +prompt('Digite um numero para ver a tabuada.')
+
+const numero = [
+tabuada, 
+tabuada, 
+tabuada, 
+tabuada, 
+tabuada, 
+tabuada, 
+tabuada, 
+tabuada, 
+tabuada, 
+tabuada,
+]
+
+
+for(let i in numero){
+
+console.log(`${numero[i]} x ${+i + 1} = ${((+i)+ 1) * numero[i]}`)
+    
+}
---
+++ exercicio3/script.js
@@ -0,0 +1,8 @@
+const strings = ['FaZe', 'Natus Vincere', 'Cloud9', 'Astralis', 'Vitality', 'Movistar Riders', 'G2', 'ENCE', 'Ninjas in Pyjamas', 'MOUZ']
+
+
+  for (let [i, j] of strings.entries()){ 
+  
+    console.log(`${(i + 1)} - ${j}`)
+  
+  }
