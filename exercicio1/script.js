9d7e75ea772477e3849df86519c20f9c34483df3
LorenzoBaridotti <lorenzo.baridotti@hotmail.com>
2022-08-18 21:14:47 -0300

    Exercícios a016

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -5,6 +5,7 @@ const array = [
     [20, 33, 11, 12, 19],
     [3, 3, 4, 5, 10]
 ]
+// VERSÃO COM FOR IN E FOR OF
 
 for(let i in array){
       let jogador = `Jogador ${+i + 1}: `
@@ -13,3 +14,9 @@ for(let i in array){
       }
   console.log(jogador)
     }
+
+//VERSÃO COM FOR COMUM
+
+    for(let i = 0; i < array.length; i++){
+      console.log(`Jogador ${i + 1}: ${array[i]}`)
+  }
