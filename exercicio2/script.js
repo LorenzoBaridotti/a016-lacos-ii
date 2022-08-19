303495765fbae6738bcd580939427dde1db6b754
LorenzoBaridotti <lorenzo.baridotti@hotmail.com>
2022-08-18 21:06:40 -0300

    Exercícios a016

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,21 +1,10 @@
 let tabuada = +prompt('Digite um numero para ver a tabuada.')
 
-const numero = [
-tabuada, 
-tabuada, 
-tabuada, 
-tabuada, 
-tabuada, 
-tabuada, 
-tabuada, 
-tabuada, 
-tabuada, 
-tabuada,
-]
+const numero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 for(let i in numero){
 
-console.log(`${numero[i]} x ${+i + 1} = ${((+i)+ 1) * numero[i]}`)
+console.log(`${tabuada} x ${numero[i]} = ${((+i)+ 1) * tabuada}`)
     
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,8 +1,8 @@
 const strings = ['FaZe', 'Natus Vincere', 'Cloud9', 'Astralis', 'Vitality', 'Movistar Riders', 'G2', 'ENCE', 'Ninjas in Pyjamas', 'MOUZ']
+let num = 1
 
-
-  for (let [i, j] of strings.entries()){ 
+  for (let i of strings){ 
   
-    console.log(`${(i + 1)} - ${j}`)
+    console.log(`${[num++]} - ${i}`)
   
   }
